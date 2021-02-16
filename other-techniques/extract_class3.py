ed711062d104f3bd615fac724e24cacf2e67e38f
GraySpace <alexandergray37@gmail.com>
2021-02-16 15:52:05 -0500

    other techniques done

--- other-techniques/extract_class3.py
+++ other-techniques/extract_class3.py
@@ -4,32 +4,32 @@ WELL_DONE = 3000
 MEDIUM = 2500
 COOKED_CONSTANT = 0.05
 
+class Cooking:
+    def __init__(self, time, temperature, pressure, desired_state):
+        self.time = time
+        self.temperature = temperature
+        self.pressure = pressure
+        self.desired_state = desired_state
+        
+    def is_criteria_satisfied(self):
+        return self.is_medium() or self.is_well_done()
+    
+    def is_well_done(self):
+        return self.desired_state == "well-done" and self.cooking_progress() >= WELL_DONE
+    
+    def cooking_progress(self):
+        return self.time * self.temperature * self.pressure * COOKED_CONSTANT
+    
+    def is_medium(self):
+        return self.desired_state == "medium" and self.cooking_progress() >= MEDIUM
+    
+    def is_done_cooking(self):
+        if self.is_criteria_satisfied():
+            print('cooking is done')
+        else:
+            print('keep cooking')
+    
+    
+alex_steak = Cooking(30, 100, 20, "well-done")
 
-def is_cookeding_criteria_satisfied(time, temperature, 
-                                    pressure, desired_state):
-    return is_well_done(time, temperature, pressure, desired_state) or \
-           is_medium(time, temperature, pressure, desired_state)
-
-
-def is_well_done(time, temperature, pressure, desired_state):    
-    return desired_state == 'well-done' and  \
-           get_cooking_progress(time, temperature, pressure) >= WELL_DONE
-
-
-def is_medium(time, temperature, pressure, desired_state):
-    return desired_state == 'medium' and  \
-           get_cooking_progress(time, temperature, pressure) >= MEDIUM
-
-def get_cooking_progress(time, temperature, pressure):
-    return time * temperature * pressure * COOKED_CONSTANT
-
-
-time = 30 # [min]
-temp = 103 # [celcius]
-pressure = 20 # [psi]
-desired_state = 'well-done'
-
-if is_cookeding_criteria_satisfied(time, temp, pressure, desired_state):
-    print('cooking is done.')
-else:
-    print('ongoing cooking.')
+alex_steak.is_done_cooking()
